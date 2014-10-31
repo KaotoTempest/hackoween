d0c2076887507d5b5404fdb3681479ce2f32da98
KaotoTempest <uk_alien@hotmail.com>
2014-10-31 23:26:35 +0000

    Adam

---
+++ Project1/src/hack/idiotproof/Testbed.java
@@ -0,0 +1,24 @@
+package hack.idiotproof;
+
+import com.sun.rmi.rmid.ExecPermission;
+
+import java.awt.*;
+
+/**
+ * Created by Adam Bedford on 31/10/2014.
+ */
+public class Testbed {
+    private static void handleDataEvent(Event event)throws Exception
+    {
+
+    }
+
+    private static void handleOtherEvent(Event event) throws Exception
+    {
+
+    }
+
+    public static void main(String[] args){
+
+    }
+}
